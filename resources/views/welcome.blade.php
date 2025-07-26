7885be2c75e8b9bc5179d7d6d26b6418d6a15722
Abdylkadyr <abdylkadyr63@gmail.com>
2025-07-26 10:26:19 +0500

    commit 2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,6 +21,7 @@
     </head>
     <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
         <h6>Birinji commit</h6>
+        <h5>Ikinji commit</h5>
         <header class="w-full lg:max-w-4xl max-w-[335px] text-sm mb-6 not-has-[nav]:hidden">
             @if (Route::has('login'))
                 <nav class="flex items-center justify-end gap-4">
